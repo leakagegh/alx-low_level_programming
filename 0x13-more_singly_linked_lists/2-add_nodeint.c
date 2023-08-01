1abbbc4a7d8d9f2e936eb414e10f6222b2e3c6f7
leakagegh <thehealer184@gmail.com>
2023-07-31 21:58:48 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,23 +3,22 @@
 /**
  * add_nodeint - Include a new node at the beginning of a linked list
  *
- * @head: A pointer to the initial node of the list
- * @n: The data to be inserted in the new node
+ * @head: A pointer to the first node of the list
+ * @n: ata to be inserted in the new node
  *
  * Return: Pointer to the new created node, or NULL if the operation fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-if (head == NULL)
+listint_t *new;
+
+new = malloc(sizeof(listint_t));
+if (!new)
 return (NULL);
 
-listint_t *new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
+new->n = n;
+new->next = *head;
+*head = new;
 
-new_node->n = n;
-new_node->next = *head;
-*head = new_node;
-
-return (new_node);
+return (new);
 }
