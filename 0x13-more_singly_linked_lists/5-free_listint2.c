1ee927448a4cf9e1944354a0d622cf5a6bda6b95
leakagegh <thehealer184@gmail.com>
2023-08-01 13:02:58 -0700

    Releases a linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,22 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - Releases a linked list
+ * @head: Pointer to the listint_t list to be deallocated
  */
 void free_listint2(listint_t **head)
 {
+listint_t *temp;
+
+if (head == NULL)
+return;
+
 while (*head)
 {
-listint_t *temp = *head;
-*head = (*head)->next;
-free(temp);
+temp = (*head)->next;
+free(*head);
+*head = temp;
 }
+
+*head = NULL;
 }
