ea705f89408f2abf83638a828c73914b1b182c0c
leakagegh <thehealer184@gmail.com>
2023-08-07 13:38:22 -0700

    Task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,23 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, w;
+int fd, len, w;
 
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (fd == -1)
-		return (-1);
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
-	w = write(fd, text_content, len);
-	if (w < 1)
-		return (-1);
-	close(fd);
-	return (1);
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+if (fd == -1)
+return (-1);
+if (text_content == NULL)
+{
+close(fd);
+return (1);
+}
+for (len = 0; text_content[len] != '\0'; len++)
+;
+w = write(fd, text_content, len);
+if (w < 1)
+return (-1);
+close(fd);
+return (1);
 }
