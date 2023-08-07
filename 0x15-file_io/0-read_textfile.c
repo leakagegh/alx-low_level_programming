4cc237ef5ee8526ffd8d345898bdf3404e4a595b
leakagegh <thehealer184@gmail.com>
2023-08-07 13:54:18 -0700

    Task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,25 +9,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-char *buf;
-size_t r, w;
+	int fd;
+	char *buf;
+	size_t r, w;
 
-if (filename == NULL || letters < 1)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buf = malloc(letters);
-if (buf == NULL)
-return (0);
-r = read(fd, buf, letters);
-if (r < 1)
-return (0);
-w = write(STDOUT_FILENO, buf, r);
-if (w < 1)
-return (0);
-free(buf);
-close(fd);
-return (w);
+	if (filename == NULL || letters < 1)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
+	r = read(fd, buf, letters);
+	if (r < 1)
+		return (0);
+	w = write(STDOUT_FILENO, buf, r);
+	if (w < 1)
+		return (0);
+	free(buf);
+	close(fd);
+	return (w);
 }
