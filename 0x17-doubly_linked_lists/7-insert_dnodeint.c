732e1bb21710343f08ff806bd0e58f18b2f8ad48
leakagegh <thehealer184@gmail.com>
2023-09-14 12:41:42 -0700

    update_7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,43 +9,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int c_index;
-	dlistint_t *node, *prev, *next;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (h == NULL)
-		return (NULL);
-
+	new = NULL;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	prev = *h;
-	c_index = 0;
-	while (prev)
+		new = add_dnodeint(h, n);
+	else
 	{
-		if (c_index == idx - 1)
-			break;
-		++c_index;
-		prev = prev->next;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
 
-	if (prev == NULL)
-		return (NULL);
-
-	if (prev->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	next = prev->next;
-
-	node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
-		return (NULL);
-
-	node->n = n;
-	node->prev = prev;
-	node->next = next;
-
-	prev->next = node;
-	next->prev = node;
-
-	return (node);
+	return (new);
 }
