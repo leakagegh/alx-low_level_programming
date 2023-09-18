c302a0c7fcc0d0258ad8143bd227042e440d42a3
leakagegh <thehealer184@gmail.com>
2023-09-18 12:45:53 -0700

    0.

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * @c: char to check
+ * Return: 1 for upper letter or 0 for any one else
+ */
+
+int _isupper(int c)
+
+{
+if (c >= 'A' && c <= 'Z')
+
+return (1);
+
+else
+
+return (0);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: the number to be commuted..
+ * Return: Absolute value of number or zero
+ */
+
+int _abs(int n)
+
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
